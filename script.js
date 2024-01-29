578470b75316405739c2d8fce65051f103f9282b
EreminNikita <eremin.learn@gmail.vom>
2024-01-29 22:18:04 +0400

    new

---
+++ script.js
@@ -0,0 +1,38 @@
+// ! 8
+
+/* function greet(name){
+    return `Hello, ${name} how are you doing today?`
+  } */
+
+
+// ! 8
+
+/* function reverseList(list) {
+
+    console.log(list.reverse());
+}
+
+reverseList([1, 'test', true]); */
+
+
+// !8
+
+/* function arrFix(num) {
+
+    var minVal = num[0];
+    var maxVal = num[num.length - 1];
+
+    const arrNew = [];
+
+    var current = minVal;
+
+
+    while (current <= maxVal) {
+        arrNew.push(current);
+        current++;
+    }
+    console.log(arrNew);
+
+}
+
+arrFix([1, 7]); */
