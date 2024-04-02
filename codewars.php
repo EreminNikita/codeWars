6542e66e74fe9fe5e3d1a038b6b15032849c163f
EreminNikita <eremin.learn@gmail.com>
2024-04-02 16:37:32 +0400

    phpstorm

--- codewars.php
+++ codewars.php
@@ -1,15 +1,16 @@
 <?php
 
-/*$array = [1, 2, 3, 4, 'str'];
+$array = [1, 2, 3, 4, 'str'];
 $count = 0;
 foreach ($array as $value)
     if (is_numeric($value)) {
         $count = $count + $value;
     }
-echo $count;*/
+echo $count;
 
 $array = [1, 2, 3, 4, -5];
 foreach ($array as $value) {
     echo -$value;
 }
 var_dump($array);
+//phpstorm
