b1b8fc5d3e009538f0039dbe263cd009bcb40ce3
EreminNikita <eremin.learn@gmail.com>
2024-04-03 21:32:04 +0400

    03.04.24

--- codewars.php
+++ codewars.php
@@ -1,16 +1,31 @@
 <?php
 
-$array = [1, 2, 3, 4, 'str'];
+/*$array = [1, 2, 3, 4, 'str'];
 $count = 0;
 foreach ($array as $value)
     if (is_numeric($value)) {
         $count = $count + $value;
     }
-echo $count;
+echo $count;*/
 
-$array = [1, 2, 3, 4, -5];
-foreach ($array as $value) {
-    echo -$value;
+/*function evenOrOdd($number) {
+    if ($number % 2 == 0) {
+        return "Even";
+    } else {
+        return "Odd";
+    }
 }
-var_dump($array);
-//phpstorm
+evenOrOdd(5);*/
+
+/*function removeExclamationMarks($string) {
+    echo (str_replace('!', '', $string));
+    return str_replace('!', '', $string);
+
+}
+removeExclamationMarks('He!!o wor!d');*/
+
+//function sumArray($array) {
+//    echo array_sum($array)-max($array)-min($array);
+//}
+//
+//sumArray([1,2,5,10]);
