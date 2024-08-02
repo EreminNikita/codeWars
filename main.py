6e60847814419f58a381c29447c7ed0b687232d0
EreminNikita <eremin.learn@gmail.com>
2024-08-02 20:53:59 +0400

    02.08

---
+++ main.py
@@ -0,0 +1,37 @@
+# def invert(lst):
+#     print([-x for x in lst])
+#
+#
+# invert([1, 2, 3])
+
+
+# def difference_in_ages(ages):
+#     young = min(ages)
+#     old = max(ages)
+#     difference = old - young
+#     return (young, old, difference)
+#
+#
+# print(difference_in_ages([6, 23, 56]))
+
+
+# def sorter(textbooks):
+#     return sorted(textbooks, key=lambda x: x.lower())
+#
+#
+# print(sorter(['Bio', 'Math', 'history']))
+
+
+# import heapq
+#
+#
+# def merge_arrays(arr1, arr2):
+#     merged = heapq.merge(arr1, arr2)
+#
+#     result = sorted(set(merged))
+#     return result
+#
+#
+# arr1 = [1, 3, 5, 7]
+# arr2 = [2, 3, 6, 8]
+# print(merge_arrays(arr1, arr2))
